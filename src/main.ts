ce440f2c296c81f25b9db02ce0dce9603fceceaa
youssef-khalifa <ykhalifa19921@gmail.com>
2024-09-08 03:54:19 +0300

    s

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-
+import './tutorial.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
